2e66ec5ab54e7dd810e40fcb206ebbf46c96b907
broisnischal <neeswebservice@gmail.com>
2024-09-29 19:23:20 +0545

    feat:changes

--- app/page.tsx
+++ app/page.tsx
@@ -30,31 +30,31 @@ export default function ReactNepal() {
     },
   ];
 
-  const sponsors = [
-    {
-      name: "RONT",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz5ECw1gcllCjAYQXo75qW6gnuvJmSFk6lQ&s",
-    },
-    {
-      name: "Kathmandu IT",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-wAdrBfysUvWN8O554diHTZSdb14KOH2yeQ&s",
-    },
-    {
-      name: "Nepal Innovates",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/5/54/Tribhuvan_University_logo.svg",
-    },
-    {
-      name: "Nepal Innovates",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/5/54/Tribhuvan_University_logo.svg",
-    },
-  ];
+  // const sponsors = [
+  //   {
+  //     name: "RONT",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz5ECw1gcllCjAYQXo75qW6gnuvJmSFk6lQ&s",
+  //   },
+  //   {
+  //     name: "Kathmandu IT",
+  //     image:
+  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-wAdrBfysUvWN8O554diHTZSdb14KOH2yeQ&s",
+  //   },
+  //   {
+  //     name: "Nepal Innovates",
+  //     image:
+  //       "https://upload.wikimedia.org/wikipedia/en/5/54/Tribhuvan_University_logo.svg",
+  //   },
+  //   {
+  //     name: "Nepal Innovates",
+  //     image:
+  //       "https://upload.wikimedia.org/wikipedia/en/5/54/Tribhuvan_University_logo.svg",
+  //   },
+  // ];
 
   return (
-    <div className="w-full items-center flex flex-col min-h-screen">
+    <div className="  w-full items-center flex flex-col min-h-screen">
       <header className=" items-center justify-center w-full m-auto top-0 z-50 ">
         <div className="flex  justify-center h-14 items-center">
           <div className="mr-4 hidden md:flex">
@@ -381,7 +381,7 @@ export default function ReactNepal() {
           </div>
         </div>
       </section>
-      <section
+      {/* <section
         id="sponsors"
         className="w-full border-t py-12 md:py-24 lg:py-32 "
       >
@@ -406,8 +406,9 @@ export default function ReactNepal() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
+      <Community />
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-muted-foreground">
           © 2024 React Nepal. All rights reserved.
@@ -422,5 +423,173 @@ export default function ReactNepal() {
         </nav>
       </footer>
     </div>
+  );
+}
+
+function Community() {
+  return (
+    <section className=" gap-2 w-full flex flex-col items-center px-8 bg-black text-white py-20">
+      <div className="max-w-4xl flex flex-col gap-8">
+        <div>
+          <div className="flex flex-col gap-2 justify-center w-full items-start">
+            <div className="inline-flex items-start">
+              <h1 className="tracking-tight inline font-semibold text-4xl lg:text-5xl">
+                Community
+              </h1>
+              &nbsp;&nbsp;
+            </div>
+          </div>
+          <p className="w-full my-2 text-lg lg:text-xl font-normal text-default-500 max-w-full md:w-full items-start">
+            Get involved in our community. Everyone is welcome!
+          </p>
+          <br />
+          <span
+            aria-hidden="true"
+            className="w-px h-px block"
+            // style="margin-left: 0.25rem; margin-top: 3rem;"
+          ></span>
+          <div className="grid *:border grid-cols-1 md:grid-cols-2 gap-8 lg:grid-cols-3">
+            <div
+              className="flex border flex-col relative overflow-hidden h-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 text-white data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large cursor-pointer transition-transform-background motion-reduce:transition-none data-[pressed=true]:scale-[0.97] tap-highlight-transparent border-transparent bg-white/5 dark:bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]"
+              role="button"
+            >
+              <div className="flex  p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-large gap-2 pb-0">
+                <div className="flex justify-center p-2 rounded-full items-center text-pink-500 bg-transparent">
+                  <svg
+                    height="32"
+                    viewBox="0 0 24 24"
+                    width="32"
+                    className="text-[#00ACEE]"
+                  >
+                    <path
+                      d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+                <p className="text-base font-semibold">Twitter</p>
+                <svg
+                  aria-hidden="true"
+                  fill="none"
+                  focusable="false"
+                  height="18"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="18"
+                  className="text-white"
+                >
+                  <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+                  <path d="M15 3h6v6"></path>
+                  <path d="M10 14L21 3"></path>
+                </svg>
+              </div>
+              <div className="relative flex w-full p-3 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased pt-0">
+                <p className="font-normal text-base text-default-500">
+                  For announcements, tips and general information.
+                </p>
+              </div>
+            </div>
+            <button
+              className="flex flex-col relative overflow-hidden h-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large cursor-pointer transition-transform-background motion-reduce:transition-none data-[pressed=true]:scale-[0.97] tap-highlight-transparent border-transparent bg-white/5 text-white  backdrop-blur-lg backdrop-saturate-[1.8]"
+              type="button"
+              role="button"
+            >
+              <div className="flex p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-large gap-2 pb-0">
+                <div className="flex justify-center p-2 rounded-full items-center text-pink-500 bg-transparent">
+                  <svg
+                    height="32"
+                    viewBox="0 0 24 24"
+                    width="32"
+                    className="text-[#7289DA]"
+                  >
+                    <path
+                      d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+                <p className="text-base font-semibold">Discord</p>
+                <svg
+                  aria-hidden="true"
+                  fill="none"
+                  focusable="false"
+                  height="18"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="18"
+                  className="text-white"
+                >
+                  <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+                  <path d="M15 3h6v6"></path>
+                  <path d="M10 14L21 3"></path>
+                </svg>
+              </div>
+              <div className="relative flex w-full p-3 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased pt-0">
+                <p className="font-normal text-base text-default-500">
+                  To get involved in the community, ask questions and share
+                  tips.
+                </p>
+              </div>
+            </button>
+            <button
+              className="flex flex-col relative overflow-hidden h-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large cursor-pointer transition-transform-background motion-reduce:transition-none data-[pressed=true]:scale-[0.97] tap-highlight-transparent border-transparent bg-white/5 text-white dark:bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]"
+              type="button"
+              role="button"
+            >
+              <div className="flex p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-large gap-2 pb-0">
+                <div className="flex justify-center p-2 rounded-full items-center text-pink-500 bg-transparent">
+                  <svg
+                    height="32"
+                    viewBox="0 0 24 24"
+                    width="32"
+                    className="text-[#E7E7E7] dark:text-[#E7E7E7]"
+                  >
+                    <path
+                      clip-rule="evenodd"
+                      d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+                <p className="text-base font-semibold">Github</p>
+                <svg
+                  aria-hidden="true"
+                  fill="none"
+                  focusable="false"
+                  height="18"
+                  shape-rendering="geometricPrecision"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  width="18"
+                  className="text-white"
+                >
+                  <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+                  <path d="M15 3h6v6"></path>
+                  <path d="M10 14L21 3"></path>
+                </svg>
+              </div>
+              <div className="relative flex w-full p-3 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased pt-0">
+                <p className="font-normal text-base text-default-500">
+                  To report bugs, request features and contribute to the
+                  project.
+                </p>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
